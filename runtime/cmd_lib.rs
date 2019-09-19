f342ad8b50001b88f1f128934e8244a321955f53
Tao Guo <glorioustao@gmail.com>
2019-09-19 11:48:52 -0700

    Update rust macros

--- runtime/cmd_lib.rs
+++ runtime/cmd_lib.rs
@@ -1,7 +1,6 @@
 use std::io::{Error, ErrorKind};
 use std::process;
 use std::process::ExitStatus;
-use std::str;
 use std::fmt::Display;
 
 pub type FunResult = Result<String, std::io::Error>;
@@ -22,39 +21,32 @@ macro_rules! output {
 }
 
 #[macro_export]
-macro_rules! _run_cmd {
-    ($cmd:expr) => (run_cmd($cmd, &[]));
-    ($cmd:expr, $($arg:expr),+) =>
-        (run_cmd($cmd, &[$($arg),+]));
-}
-
-#[macro_export]
 macro_rules! run_cmd {
     ($($arg:tt)*) => {
-        _run_cmd!(format!($($arg)*).as_ref())
+        run_cmd(&format!($($arg)*).split_whitespace().collect::<Vec<&str>>());
     }
 }
 
 #[macro_export]
 macro_rules! run_fun {
-    ($cmd:expr) => (run_fun($cmd, &[]));
-    ($cmd:expr, $($arg:expr),+) =>
-        (run_fun($cmd, &[$($arg),+]));
+    ($($arg:tt)*) => {
+        run_fun(&format!($($arg)*).split_whitespace().collect::<Vec<&str>>());
+    }
 }
 
-pub fn info<S>(msg: S) -> CmdResult where S: Into<String> + Display {
+pub fn info<S>(msg: S) where S: Into<String> + Display {
     eprintln!("{}", msg);
-    Ok(())
 }
 
-pub fn output<S>(msg: S) -> FunResult where S: Into<String> {
-    Ok(msg.into())
+pub fn output<S>(msg: S) -> String where S: Into<String> {
+    msg.into()
 }
 
-pub fn run_cmd(command: &str, args: &[&str]) -> CmdResult {
-    info!("Running {} ...", command)?;
+pub fn run_cmd(full_command: &[&str]) -> CmdResult {
+    info!("Running {:?} ...", full_command);
+    let command = &full_command[0];
     let status = process::Command::new(command)
-                                  .args(args)
+                                  .args(&full_command[1..])
                                   .status()?;
     if !status.success() {
         Err(to_io_error(command, status))
@@ -63,8 +55,12 @@ pub fn run_cmd(command: &str, args: &[&str]) -> CmdResult {
     }
 }
 
-pub fn run_fun(command: &str) -> FunResult {
-    let output = process::Command::new(command).output()?;
+pub fn run_fun(full_command: &[&str]) -> FunResult {
+    info!("Running {:?} ...", full_command);
+    let command = &full_command[0];
+    let output = process::Command::new(command)
+                                  .args(&full_command[1..])
+                                  .output()?;
     if ! output.status.success() {
         Err(to_io_error(command, output.status))
     } else {
@@ -80,3 +76,4 @@ fn to_io_error(command: &str, status: ExitStatus) -> Error {
         Error::new(ErrorKind::Other, "Unknown error")
     }
 }
+
